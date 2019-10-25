1df134bb0d9e2e73c6013c079515e7bfae98707e
thepudding <54706068+aanoguchi@users.noreply.github.com>
2019-10-24 22:15:58 -0700

    Added Quiz 7

---
+++ Quizzes/Q7.cpp
@@ -0,0 +1,37 @@
+/**
+ * Quiz 7
+ * Strings
+ * 1. Read the comments given for `averageWordLen`
+ *    Imagine a function which would help you implement it.
+ *    Write the name and purpose statement for your imaginary function.
+ * 2. Implement `averageWordLen`
+ **/
+
+#include <string>
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+// Calculates the average length of each word in a string
+// Input
+// s - a string of words separated by whitespace
+// Output
+// the average length of each word as a 
+double averageWordLen(string s);
+void test();
+
+int main() {
+  test();
+  cout << //TODO
+  return 0;
+}
+
+void test() {
+  // On paper, write your tests in the form `a -> b`
+  // corresponding to: assert(averageWordLen(a) == b);
+  return;
+}
+double averageWordLen(string s) {
+  return 0;
+}
